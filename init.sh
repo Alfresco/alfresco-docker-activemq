259c0616b00ca66de0999c832db9e50bd5da95d3
Sergiu Vidrascu <sergiu.vidrascu@ness.com>
2021-04-09 13:59:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,8 +10,6 @@ sed -i "s/guest.*//g" ${ACTIVEMQ_HOME}/conf/credentials.properties
 
 # Change admin password if set vie env variable
 if [ ! -z "${ACTIVEMQ_ADMIN_LOGIN}" ] && [ ! -z "${ACTIVEMQ_ADMIN_PASSWORD}" ]; then
-    regexp: "127.0.0.1"
-    replace: "0.0.0.0"
   sed -i "s/127.0.0.1/0.0.0.0/g" ${ACTIVEMQ_HOME}/conf/jetty.xml
   sed -i "s/admin=.*/"${ACTIVEMQ_ADMIN_LOGIN}"="${ACTIVEMQ_ADMIN_PASSWORD}"/g" ${ACTIVEMQ_HOME}/conf/users.properties
   sed -i "s/admin.*/"${ACTIVEMQ_ADMIN_LOGIN}": "${ACTIVEMQ_ADMIN_PASSWORD}", admin/g" ${ACTIVEMQ_HOME}/conf/jetty-realm.properties
